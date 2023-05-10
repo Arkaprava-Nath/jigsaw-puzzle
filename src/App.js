1eb9509fec9f5cbf5565b600912ade7fb5e23c92
Arkaprava Nath <natharkaprava@gmail.com>
2023-05-10 18:11:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
-
+import { JigsawPuzzle } from "react-jigsaw-puzzle/lib";
+import "react-jigsaw-puzzle/lib/jigsaw-puzzle.css";
+import img from "./sq.png";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+     const [text, setText] = useState("Unpuzzle the pieces!!");
+      
+    const set = () => {
+        setText("Congratulations!!");
+    };
+      
+    return (
+        <>
+            <h2 className="tag">{text}</h2>
+            <div class="flex-container">
+            <JigsawPuzzle
+                imageSrc={img}
+                rows={4}
+                columns={4}
+                onSolved={set}
+                className="jigsaw-puzzle"
+            />
+            <div className="ori">
+              
+              <img src={img}   />
+            </div>
+            </div>
+        </>
+    );
+ 
 }
 
 export default App;
